f722d155b3ddd7a5fd03a35f18713618687c1c2a
amukash <143036481+amukash@users.noreply.github.com>
2024-10-25 15:09:26 -0500

    fixing submission account

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,24 @@
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
-    $('html').removeClass('nojs');
-    $('html').addClass('hasjs');
-    $('#email').on('focus', function() {
-        $('#input-email label').addClass('active');
-    });
-    $('#email').on('blur', function() {
-      if($('#email').val().length == 0) {
-        $('#input-email label').removeClass('active');
-      }
-    });
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+  $('#email').on('focus', function() {
+    $('#input-email label').addClass('active');
+  });
+  $('#email').on('blur', function() {
+    if ($('#email').val().length == 0) {
+      $('#input-email label').removeClass('active');
+    }
+  });
+});
+
+  // JavaScript to handle form submission
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Optionally, you can handle form data here (e.g., validation, AJAX submission)
+
+    // Reset the form fields
+    this.reset(); // Resets the form fields to their initial values
+
   });
